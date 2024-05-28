a5d6a79acbcf4186c7c938ea686c029889c7d684
Ian Kamau <kian99564@gmail.com>
2024-05-28 23:05:48 +0300

    feat: implemented google oauth authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,34 +5,39 @@ import "./globals.css";
 import { Provider } from "react-redux";
 import store from "@/lib/store";
 import Head from "next/head";
+import { SessionProvider } from "next-auth/react";
+import Navbar from "@/components/navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
+  session,
 }: Readonly<{
   children: React.ReactNode;
+  session: any; // Add session type if known
 }>) {
   return (
-    <>
+    <html lang="en">
       <Head>
         <title>SIL 2.0</title>
         <meta
           name="description"
-          content="All-in-one platform to create content, generate captions with Al, and schedule your
-              posts on social media!"
+          content="All-in-one platform to create content, generate captions with Al, and schedule your posts on social media!"
         />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
-        {/* <link rel="shortcut icon" href="assets/images/logo.svg" /> */}
       </Head>
-      <html lang="en">
+      <body className={inter.className}>
         <Provider store={store}>
-          <body className={inter.className}>{children}</body>
+          <SessionProvider session={session} refetchInterval={2 * 60}>
+            <Navbar />
+            {children}
+          </SessionProvider>
         </Provider>
-      </html>
-    </>
+      </body>
+    </html>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useSession, signOut } from "next-auth/react";
+import AuthenticateButton from "../authenticateButton/AuthenticateButton";
+import { Button } from "../ui/button";
+import { AvatarProfile } from "../avator/avator";
 
 export default function Navbar() {
+  const { data: session, status } = useSession();
+  console.log(status)
   return (
-    <div>Navbar</div>
-  )
+    <div className="flex items-center justify-between p-7 mb-5 shadow-md max-[600px]:p-3 ">
+      <span className="font-bold text-2xl max-[600px]:text-sm max-[600px]:wrap">
+        Savannah Informatics - Frontend Role
+      </span>
+      {session ? (
+        <div className="flex items-center gap-4">
+          <AvatarProfile name={`${session.user!.name}`} />
+          <h4 className="max-[600px]:hidden">{session.user!.name}</h4>
+          <Button
+            onClick={() => signOut("google")}
+          >
+            Logout
+          </Button>
+        </div>
+      ) : (
+        <div>
+          <AuthenticateButton />
+        </div>
+      )}
+    </div>
+  );
 }
